f20955852e985d5688cd33e15f38bde64fb2f319
abdulselamadillmohammed <forcoding39@gmail.com>
2025-08-31 20:57:19 -0400

    implement in place manipluation

--- for_loops.cpp
+++ for_loops.cpp
@@ -1,10 +1,25 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 int main(){
+
+    vector<int> nums = {1, 2, 3};
+
     for (int i = 0; i < 10; i++){
         cout << i << "\n";
     }
 
+    for (int x : nums){
+        cout << x << " ";
+    }
+    cout << "\n";
+
+    // In place manipluation
+    for (int& x: nums){
+        x *= 2;
+    }
+
     return 0;
 }
+
