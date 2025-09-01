057a450443d435aafecbc4f35a681c56bf5d7b7d
abdulselamadillmohammed <forcoding39@gmail.com>
2025-09-01 14:06:59 -0400

    test hashing func

--- hasher.cpp
+++ hasher.cpp
@@ -5,7 +5,15 @@ using namespace std;
 
 int main() {
     hash<string> hasher;
-    
+
+    string key1 = "apple";
+    string key2 = "orange";
+
+    size_t h1 = hasher(key1);
+    size_t h2 = hasher(key2);
+
+    cout << "hash(apple) = "  << h1 << "\n";
+    cout << "hash(orange) = " << h2 << "\n"; 
 
     return 0;
 }
